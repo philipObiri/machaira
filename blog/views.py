e4bf243fb3406b55dcec2428da5b5026e4dd24d8
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-26 02:54:35 +0000

    feat: Added View to paginate and list all active published posts

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,19 @@ from django.views.decorators.http import require_POST
 
 
 # Create your views here.
+def post_list(request):
+    post_list = Post.published.all()
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                 'blog/post/list.html',
+                 {'posts': posts})
