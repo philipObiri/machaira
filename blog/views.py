16ca537df9ec8f122c72ad730a808f93f3adaf93
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-26 02:53:05 +0000

    feat: Initial Commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
+
 
 # Create your views here.
