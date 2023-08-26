8e9d4764f87a751dd00307e2b89c2f57323b8c2e
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-26 02:50:53 +0000

    feat: Added a custom manager to handle published posts

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
-
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
 
