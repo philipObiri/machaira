9542108b2c7ca3c836d2d81de807a8d8f7edd7d1
Philip Obiri <philipobiri3.1@gmail.com>
2023-08-26 02:56:39 +0000

    feat: Added a class Based view for the Post List

--- blog/views.py
+++ blog/views.py
@@ -46,3 +46,14 @@ def post_detail(request, year, month, day, post):
                   {'post': post,
                    'comments': comments,
                    'form': form})
+
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
